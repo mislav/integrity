dbd0314326bdc4af7aa3a3723d172b25abaca0b7
Mislav Marohnić <mislav.marohnic@gmail.com>
2009-01-14 18:43:54 +0100

    jump through hoops to prevent webrat from loading "webrat/sinatra"
    "webrat/sinatra" tries to load "sinatra/test/methods" which doesn't exist in Sinatra 0.9.0.
    it's better to inline that code (for now) with a couple of tweaks

--- test/helpers/acceptance.rb
+++ test/helpers/acceptance.rb
@@ -1,8 +1,31 @@
-require "webrat/sinatra"
+require 'webrat/rack'
+require 'sinatra'
+require 'sinatra/test'
+
+disable :run
+disable :reload
+
+module Webrat
+  class SinatraSession < RackSession
+    include Sinatra::Test
+    
+    attr_reader :response
+
+    %w(get head post put delete).each do |verb|
+      define_method(verb) do |*args| # (path, data, headers = nil)
+        path, data, headers = *args
+        data = data.inject({}) {|data, (key,value)| data[key] = Rack::Utils.unescape(value); data }
+        params = data.merge(:env => headers || {})
+        self.__send__("#{verb}_it", path, params)
+      end
+    end
+  end
+end
+
 require Integrity.root / "app"
 require File.dirname(__FILE__) / "acceptance/git_helper"
 
-Webrat.configuration.mode = :sinatra
+Webrat.configuration.instance_variable_set("@mode", :sinatra)
 
 module AcceptanceHelper
   include FileUtils
